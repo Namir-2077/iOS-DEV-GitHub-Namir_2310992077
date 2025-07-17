d7441e915079074d1e2fdb068422b9ee23d1cf83
Namir-2077 <namir2077.be23@chitkara.edu.in>
2025-07-17 09:24:26 +0530

    Created Stuff

---
+++ Projects in Class/Trial 2/Trial 2Tests/Trial_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Trial_2Tests.swift
+//  Trial 2Tests
+//
+//  Created by student on 17/07/25.
+//
+
+import Testing
+@testable import Trial_2
+
+struct Trial_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
