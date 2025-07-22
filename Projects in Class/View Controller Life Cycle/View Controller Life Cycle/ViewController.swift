ffd5930190c28d72e8e8072124e588c08100c0cd
Namir-2077 <namir2077.be23@chitkara.edu.in>
2025-07-22 11:49:50 +0530

    Apple Pie and View Controller Life Cycle

---
+++ Projects in Class/View Controller Life Cycle/View Controller Life Cycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  View Controller Life Cycle
+//
+//  Created by student on 22/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("View is Loaded!")
+    }
+
+
+}
+
