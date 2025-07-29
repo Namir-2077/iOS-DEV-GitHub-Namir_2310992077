92635edfff9c61809e41bb5887d6175ce39cc3f6
Namir-2077 <namir2077.be23@chitkara.edu.in>
2025-07-29 09:26:09 +0530

    Practiced Calculator project
    And much more!!!!

---
+++ Practice Problem{During Class}/Calc Practice/Calc PracticeTests/Calc_PracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calc_PracticeTests.swift
+//  Calc PracticeTests
+//
+//  Created by student on 28/07/25.
+//
+
+import Testing
+@testable import Calc_Practice
+
+struct Calc_PracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
