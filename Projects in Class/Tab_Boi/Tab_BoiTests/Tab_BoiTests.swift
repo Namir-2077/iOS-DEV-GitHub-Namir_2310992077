690b0f4be0da8a64d6a161fd4a608e9a14dc68d0
Namir-2077 <namir2077.be23@chitkara.edu.in>
2025-11-03 10:06:30 +0530

    Updated

---
+++ Projects in Class/Tab_Boi/Tab_BoiTests/Tab_BoiTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tab_BoiTests.swift
+//  Tab_BoiTests
+//
+//  Created by Student on 03/11/25.
+//
+
+import Testing
+@testable import Tab_Boi
+
+struct Tab_BoiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Resonance/Resonance_Trail/Resonance_TrailTests/Resonance_TrailTests.swift
@@ -0,0 +1,17 @@
+//
+//  Resonance_TrailTests.swift
+//  Resonance_TrailTests
+//
+//  Created by Student on 31/10/25.
+//
+
+import Testing
+@testable import Resonance_Trail
+
+struct Resonance_TrailTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
